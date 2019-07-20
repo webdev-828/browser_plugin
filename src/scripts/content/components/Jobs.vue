0b1a7c344a9fa7c48419fb1b933c17552634f29f
WebStudio523 <webstudio523@gmail.com>
2019-07-21 00:37:23 +0800

    Jobs Component

--- src/scripts/content/components/Jobs.vue
+++ src/scripts/content/components/Jobs.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <div style="width: 100%; height: 100px; background-color: red"></div>
+        <div style="width: 100%; height: 150px; background-color: red"></div>
+        <hr class="v-divider v-divider--inset theme--light">
+        <div style="width: 100%; height: 150px; background-color: red"></div>
+        <hr class="v-divider v-divider--inset theme--light">
+        <div style="width: 100%; height: 150px; background-color: red"></div>
     </div>
 </template>
 
